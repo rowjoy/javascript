f999be6f9bdda2e13c48328b667a0f10159a1dd4
Jamirul islam (Joy) <69602585+rowjoy@users.noreply.github.com>
2023-03-28 08:56:16 +0000

    Day - 3  4 exam and test class 1 2,3

---
+++ exam-day-3.js
@@ -0,0 +1,33 @@
+console.log("Jamirul islam");
+/*
+Chapter - 1 video no -5 , Question solved 
+*/
+// Question - 1 
+// Q1 add string and number 
+let a = "Jamirul islam";
+let number = 10245;
+console.log(a + number);
+//Question - 2 
+//Q2 q1 data typeof print 
+console.log(typeof (a + number));
+
+// Question - 3
+// Q3 - const add new object 
+const student = {
+  name: "Jamirul islam",
+  roll: 15,
+  isPass: false,
+};
+
+student["totalNUmber"] = 215;
+student["passSubject"] = 5;
+
+console.log(student);
+
+// Q4  created dictionary 
+ const dictionary = {
+   name : "My name is jamirul islam joy",
+   roll : "My class roll in ten",
+   description : "Me best student in class room . My roll     do not metter in class toper ",
+ }
+console.log(dictionary.roll);
